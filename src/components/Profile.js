86d695941bfdde37edb399bdc6cb85be7b6c9af9
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-30 17:37:15 +0700

    Add Navigate component to direct to sign up if not logged in

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,18 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import { Navigate } from "react-router-dom";
 
 export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
   
   // use loggedIn to return a Navigate
-
+  if (!loggedIn) {
+    return(
+      <Navigate to='/sign-up' />
+    );
+  }
   return (
     <main>
       <h1>{currentUser.username}</h1>
